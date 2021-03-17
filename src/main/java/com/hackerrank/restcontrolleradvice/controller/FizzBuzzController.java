1fbbf2289b2d61eda3a48a50775429e1434cae18
Julian Gomez <madknight@gmail.com>
2021-03-16 19:19:00 -0500

    Second commit

--- src/main/java/com/hackerrank/restcontrolleradvice/controller/FizzBuzzController.java
+++ src/main/java/com/hackerrank/restcontrolleradvice/controller/FizzBuzzController.java
@@ -1,45 +1,35 @@
 package com.hackerrank.restcontrolleradvice.controller;
 
-import com.hackerrank.restcontrolleradvice.dto.BuzzException;
-import com.hackerrank.restcontrolleradvice.dto.FizzBuzzException;
-import com.hackerrank.restcontrolleradvice.dto.FizzBuzzResponse;
-import com.hackerrank.restcontrolleradvice.dto.FizzException;
+import com.hackerrank.restcontrolleradvice.dto.*;
 import com.hackerrank.restcontrolleradvice.enums.FizzBuzzEnum;
-import org.springframework.http.HttpHeaders;
+import com.hackerrank.restcontrolleradvice.exception.FizzBuzzExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class FizzBuzzController {
 
-@ControllerAdvice
-public class FizzBuzzController extends ResponseEntityExceptionHandler {
+  private FizzBuzzExceptionHandler handler = new FizzBuzzExceptionHandler();
 
   @RequestMapping(value = "/controller_advice/{code}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  @ExceptionHandler({FizzException.class, BuzzException.class, FizzBuzzException.class})
-  public ResponseEntity<Object> getFizzBuzzResponse(@PathVariable("code") String code, WebRequest request)
+  public ResponseEntity<?> getFizzBuzzResponse(@PathVariable("code") String code)
+          throws FizzException, BuzzException, FizzBuzzException
   {
 
-    ResponseEntity<Object> response = null;
+
     if (FizzBuzzEnum.FIZZ.getValue().equals(code)) {
-      response = handleExceptionInternal(new FizzException("Fizz Exception has been thrown", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()),
-              new FizzBuzzResponse("Fizz Exception has been thrown", HttpStatus.INTERNAL_SERVER_ERROR.value()),
-              new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+      throw new FizzException("Fizz Exception has been thrown", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
 
     } else if (FizzBuzzEnum.BUZZ.getValue().equals(code)) {
-      response = handleExceptionInternal(new FizzException("Buzz Exception has been thrown", HttpStatus.BAD_REQUEST.getReasonPhrase()),
-              new FizzBuzzResponse("Fizz Exception has been thrown", HttpStatus.BAD_REQUEST.value()),
-              new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-
+      throw new BuzzException(
+              "Fizz Exception has been thrown", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
     } else if (FizzBuzzEnum.FIZZBUZZ.getValue().equals(code)) {
-      response = handleExceptionInternal(new FizzBuzzException("FizzBuzz Exception has been thrown", HttpStatus.INSUFFICIENT_STORAGE.getReasonPhrase()),
-              new FizzBuzzResponse("Fizz Exception has been thrown", HttpStatus.INSUFFICIENT_STORAGE.value()),
-              new HttpHeaders(), HttpStatus.INSUFFICIENT_STORAGE, request);
+      throw new FizzException(
+              "Fizz Exception has been thrown", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
     }
-    return response;
+    return ResponseEntity.ok(new FizzBuzzResponse("", HttpStatus.OK.value()));
   }
 }
--- src/main/java/com/hackerrank/restcontrolleradvice/exception/FizzBuzzExceptionHandler.java
+++ src/main/java/com/hackerrank/restcontrolleradvice/exception/FizzBuzzExceptionHandler.java
@@ -1,10 +1,36 @@
 package com.hackerrank.restcontrolleradvice.exception;
 
+import com.hackerrank.restcontrolleradvice.dto.*;
+import com.hackerrank.restcontrolleradvice.enums.FizzBuzzEnum;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class FizzBuzzExceptionHandler extends ResponseEntityExceptionHandler {
 
-  //TODO:: implement handler methods for FizzException, BuzzException and FizzBuzzException
+    @ExceptionHandler({FizzBuzzException.class, BuzzException.class, FizzBuzzException.class})
+    public ResponseEntity<Object> handleExceptions(RuntimeException exception) {
+        ResponseEntity<Object> response = null;
+        if (exception instanceof FizzException) {
+            response = handleExceptionInternal(exception,
+                    new FizzBuzzResponse("Fizz Exception has been thrown", HttpStatus.INTERNAL_SERVER_ERROR.value()),
+                    new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, null);
+
+        } else if (exception instanceof BuzzException) {
+            response = handleExceptionInternal(new BuzzException("Buzz Exception has been thrown", HttpStatus.BAD_REQUEST.getReasonPhrase()),
+                    new FizzBuzzResponse("Fizz Exception has been thrown", HttpStatus.BAD_REQUEST.value()),
+                    new HttpHeaders(), HttpStatus.BAD_REQUEST, null);
+
+        } else if (exception instanceof FizzBuzzException) {
+            response = handleExceptionInternal(new FizzBuzzException("FizzBuzz Exception has been thrown", HttpStatus.INSUFFICIENT_STORAGE.getReasonPhrase()),
+                    new FizzBuzzResponse("Fizz Exception has been thrown", HttpStatus.INSUFFICIENT_STORAGE.value()),
+                    new HttpHeaders(), HttpStatus.INSUFFICIENT_STORAGE, null);
+        }
+        return response;
+    }
 }
